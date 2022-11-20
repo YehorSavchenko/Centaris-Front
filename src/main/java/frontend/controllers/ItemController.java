5942c740db2cf271413f1b965708a0638dce23d2
Yehor <egor07030303@gmail.com>
2022-11-20 10:51:24 +0100

    Refactoring all

--- src/main/java/frontend/controllers/ItemController.java
+++ src/main/java/frontend/controllers/ItemController.java
@@ -5,15 +5,16 @@ import model.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.logging.Logger;
 
 @Controller
-public class TestController {
+public class ItemController {
 
     private final ItemsService itemsService;
 
-    public TestController(ItemsService itemsService) {
+    public ItemController(ItemsService itemsService) {
         this.itemsService = itemsService;
     }
 
@@ -24,30 +25,25 @@ public class TestController {
 
     @GetMapping("/index")
     public String getItemList(Model model){
-        Item item = itemsService.sendGetAllItemsRequest().getItems().get(0);
-        System.out.println("chuj:" + item);
-        model.addAttribute("item", item);
+        model.addAttribute("item1", itemsService.sendGetAllItemsRequest().getItems().get(0));
+        model.addAttribute("item2", itemsService.sendGetAllItemsRequest().getItems().get(1));
+        model.addAttribute("item3", itemsService.sendGetAllItemsRequest().getItems().get(2));
         return "index";
     }
 
     @GetMapping("/getItemBaseInfo")
     public String getItemBaseInfo(Model model){
-//        Item item = itemsService.sendGetAllItemsRequest().getItems().get(0);
-//        System.out.println("chuj:" + item);
-//        model.addAttribute("item", item);
         return "index";
     }
 
-    @GetMapping("/generic")
-    public String getItemInfo(){
+    @GetMapping("/{id}")
+    public String getItemInfo(@PathVariable(name = "id") long id, Model model){
+        model.addAttribute(id);
+        Item item = itemsService.getItemById(id);
+        model.addAttribute("item", item);
         return "generic";
     }
 
-    @GetMapping("/elements")
-    //TODO Nie wiem jak to nazwac
-    public String goToElements(){
-        return "elements";
-    }
 
     @GetMapping("/login")
     public String goToLogin(){
